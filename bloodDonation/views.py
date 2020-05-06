fe64bfdf3d054760430432d283ae19696ba481ad
Daniel Samson Raj <danielsam458@gmail.com>
2020-05-07 00:07:30 +0530

    updated search funtion

--- bloodDonation/views.py
+++ bloodDonation/views.py
@@ -36,7 +36,6 @@ def user_login(request):
     if request.method == 'POST':
         username = request.POST['email']
         password = request.POST['password']
-
         user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
@@ -88,13 +87,13 @@ def signup(request):
                     username=email, password=password)
                 print(user)
                 extended_user = DonarDetails(
-                    name=Name,
-                    blood_group=bloodGroup,
+                    name=Name.lower(),
+                    blood_group=bloodGroup.upper(),
                     contact_no=contactNo,
-                    area=area,
-                    city=city,
-                    state=state,
-                    country=country,
+                    area=area.lower(),
+                    city=city.lower(),
+                    state=state.lower(),
+                    country=country.lower(),
                     user=user
                 )
                 extended_user.save()
@@ -116,44 +115,122 @@ def profile(request):
     val = getVal()
     data = DonarDetails.objects.filter(user=request.user)
     val['data'] = data
+    print(data)
     return render(request, 'files/profile.html', val)
+
+
+def convert(person):
+    for i in range(len(person)):
+        if i != 1 and i != 2:
+            person[i] = person[i].capitalize()
+    return person
 
 
 def search(request):
     val = getVal()
-    val['check'] = True
-    val['initialCheck'] = False
+    val['check'] = False
+    val['check1'] = False
+    val['req_method'] = False
     if request.method == 'POST':
-        bloodGroup = request.POST['bg']
-        area = request.POST['area']
-        city = request.POST['city']
-        state = request.POST['state']
-        country = request.POST['country']
+        val['req_method'] = True
+        bloodGroup = request.POST['bg'].upper()
+        area = request.POST['area'].lower()
+        city = request.POST['city'].lower()
+        state = request.POST['state'].lower()
+        country = request.POST['country'].lower()
 
-        # fecthing data
+        val['table_headers'] = ["Name", "Blood Group", "Contact No", "Address"]
 
-        data = DonarDetails.objects.all()
-        status_check = DonarDetails.objects.filter().values()
-        available_donors = []
-        temp_list = []
-        for values in status_check:
-            temp_list.append(list(values.values()))
-        for details in temp_list:
-            if details[4] == "NO":
-                details[4] = None
-            available_donors.append(
-                [details[1], details[5], details[6], details[7]])
-        val['details'] = available_donors
+        worldwide_donors = DonarDetails.objects.filter(
+            blood_group=bloodGroup).values()
+        if len(worldwide_donors) > 0:
+            val['check1'] = True
+        countrywide_donors = DonarDetails.objects.filter(
+            country=country,
+            blood_group=bloodGroup
+        ).values()
+        statewide_donors = DonarDetails.objects.filter(
+            country=country,
+            state=state,
+            blood_group=bloodGroup
+        ).values()
+        citywide_donors = DonarDetails.objects.filter(
+            country=country,
+            state=state,
+            city=city,
+            blood_group=bloodGroup
+        ).values()
+        areawide_donors = DonarDetails.objects.filter(
+            country=country,
+            state=state,
+            city=city,
+            area=area,
+            blood_group=bloodGroup
+        ).values()
 
-        # print(val['details'])
+        check_dup = {}
 
-        val['table_headers'] = ["Name",
-                                "Blood Group", "Contact No", "Address"]
-        val['initialCheck'] = True
+        # print(countrywide_donors)
+
+        filter1 = []
+        for value in areawide_donors:
+            person = list(value.values())[1:-1]
+            if person[2] not in check_dup:
+                finalList = convert(person)
+                check_dup[person[2]] = True
+                finalList1 = finalList[:3]
+                finalList1.append(",".join(finalList[3:]))
+                filter1.append(finalList1)
+                # print("filter1 ", filter1)
+
+        filter2 = []
+        for value in citywide_donors:
+            person = list(value.values())[1: -1]
+            if person[2] not in check_dup:
+                check_dup[person[2]] = True
+                finalList = convert(person)
+                finalList1 = finalList[:3]
+                finalList1.append(",".join(finalList[3:]))
+                filter2.append(finalList1)
+                # print("filter2 ", filter2)
+
+        filter3 = []
+        for value in statewide_donors:
+            person = list(value.values())[1: -1]
+            if person[2] not in check_dup:
+                check_dup[person[2]] = True
+                finalList = convert(person)
+                finalList1 = finalList[:3]
+                finalList1.append(",".join(finalList[3:]))
+                filter3.append(finalList1)
+                # print("filter3 ", filter3)
+
+        filter4 = []
+        for value in countrywide_donors:
+            person = list(value.values())[1: -1]
+            if person[2] not in check_dup:
+                check_dup[person[2]] = True
+                finalList = convert(person)
+                finalList1 = finalList[:3]
+                finalList1.append(",".join(finalList[3:]))
+                filter4.append(finalList1)
+                # print("filter4 ", filter4)
+        filter5 = []
+        for value in worldwide_donors:
+            person = list(value.values())[1: -1]
+            if person[2] not in check_dup:
+                check_dup[person[2]] = True
+                finalList = convert(person)
+                finalList1 = finalList[:3]
+                finalList1.append(",".join(finalList[3:]))
+                filter5.append(finalList1)
+
+        val['details'] = [filter1, filter2, filter3, filter4, filter5]
+        val['check'] = True
+        return render(request, 'files/search.html', val)
     else:
         val['details'] = None
-        val['len'] = []
-    return render(request, 'files/search.html', val)
+        return render(request, 'files/search.html', val)
 
 
 @login_required(login_url='/login/')
