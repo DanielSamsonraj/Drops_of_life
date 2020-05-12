5809d74f50dc53cccaaf2e95a1061ab2d1e08f2d
Daniel Samson Raj <danielsam458@gmail.com>
2020-05-13 00:08:06 +0530

    Update settings.py

--- MiniProject/settings.py
+++ MiniProject/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'MyDatabase',
         'USER': 'postgres',
-        'PASSWORD': 'BhaVya',
+        'PASSWORD': 'cse1',
         'HOST': 'localhost'
     }
 }
@@ -123,12 +123,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'danielsam458@gmail.com'
-EMAIL_HOST_PASSWORD = 'jiszilasjrizshud'
+EMAIL_HOST_PASSWORD = 'pjiejpugvuathhgr'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'DropsOfLife Team <noreply@DropsOfLife.com>'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
